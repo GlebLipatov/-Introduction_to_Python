8f88070c60abbb82cd98fd960a1d6a1c74f793c5
Gleb Lipatov <it.lipatov@gmail.com>
2023-04-21 19:22:08 +0300

    Homework 4 is done v2.0

--- S_4/Task_22.py
+++ S_4/Task_22.py
@@ -2,15 +2,17 @@ import random
 
 def creator_set(length: int):
     new_set = set()
-    MAX_VALUE = 9
+    MAX_VALUE = 100
     for i in range(length):
         new_set.add(random.randint(0, MAX_VALUE))
     return new_set
 
 set_1 = creator_set(int(input('Размер первого множества: ')))
 set_2 = creator_set(int(input('Размер второго множества: ')))
-set_3 = set().union(set_1, set_2)
+
+new_list = list(set().union(set_1, set_2))
+new_list.sort()
 
 print(set_1)
 print(set_2)
-print(set_3)
+print(new_list)
