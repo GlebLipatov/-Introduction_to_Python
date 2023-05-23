c62d509d1665964079627fa52a6a96ec5e5125f7
Gleb Lipatov <it.lipatov@gmail.com>
2023-05-23 15:01:20 +0300

    Task 36 fix.

--- S_7/Task_36.py
+++ S_7/Task_36.py
@@ -1,7 +1,11 @@
+import random
 def print_operation_table(operation, num_rows=6, num_columns=6):
     for row in range(1, num_rows + 1):
         for column in range(1, num_columns + 1):
             print(f'{operation(row, column)}', end=' ')
         print('\n')
 
-print_operation_table(lambda x, y: x * y, 10, 10)
+rows = random.randint(1, 10)
+columns = random.randint(1, 10)
+print(f'Строк: {rows}\nСтолбцов: {columns}\n')
+print_operation_table(lambda x, y: x * y, rows, columns)
